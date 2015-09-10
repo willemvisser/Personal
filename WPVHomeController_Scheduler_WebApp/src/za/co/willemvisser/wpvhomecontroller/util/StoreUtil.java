4e1350244832caac58b3f0def0c8d00a34509776
willemv <willem.visser@gmail.com>
2015-09-10 07:02:30 +0200

    Added StoreUtil

---
+++ WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/util/StoreUtil.java
@@ -0,0 +1,56 @@
+package za.co.willemvisser.wpvhomecontroller.util;
+
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.log4j.Logger;
+
+import za.co.willemvisser.wpvhomecontroller.config.ConfigController;
+import za.co.willemvisser.wpvhomecontroller.config.dto.GeneralPropertyDTO;
+
+public enum StoreUtil {
+
+	INSTANCE;
+	
+	static Logger log = Logger.getLogger(StoreUtil.class.getName());
+	
+	public void logEvent(String deviceId, String command, Boolean success, String description, Date timestamp) {
+		log.info("logEvent: " + description);
+		
+		GeneralPropertyDTO propStoreEventURL = ConfigController.INSTANCE.getGeneralProperty(GeneralPropertyDTO.PROP_STORE_EVENT_URL);
+		if (propStoreEventURL == null) {
+			log.error("Could not find the Store HTTP URl in the general properties file under key: " + GeneralPropertyDTO.PROP_STORE_EVENT_URL);
+			//TODO - add to event??
+			return;
+		}
+		
+		
+		
+		try {
+			StringBuffer requestBuffer = new StringBuffer(propStoreEventURL.getValue());
+			requestBuffer.append(deviceId);
+			requestBuffer.append("/");
+			requestBuffer.append(command);
+			requestBuffer.append("/");
+			requestBuffer.append(success);
+			requestBuffer.append("/");
+			requestBuffer.append(URLEncoder.encode(description, "UTF-8").replaceAll("\\+", "%20"));
+			//TODO - timestamp in case the post get's delayed?
+			
+			HttpResponse response = HttpUtil.INSTANCE.doHttpGet(requestBuffer.toString());
+			StringBuffer responseStr = HttpUtil.INSTANCE.getResponseContent(response);
+			if (responseStr.toString().equals("OK")) {
+				log.info("Posted successfully.");
+			} else {
+				log.error("Failure.");
+				//TODO - we should add this to the log and retry later
+			}
+		} catch (Exception e) {
+			log.error("Could not post event: " + e);
+			
+			//TODO - we should store this event and try again later
+		}
+	}
+}
