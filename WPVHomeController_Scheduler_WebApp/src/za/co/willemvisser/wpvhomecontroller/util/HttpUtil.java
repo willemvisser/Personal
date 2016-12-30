2a62f460831667b353e11429d9300d0d7f8ff8d5
Visser <willemv@amazon.com>
2016-12-30 10:02:11 +0200

    Enhanced HttpUtil to use Chrome user agent.

--- WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/util/HttpUtil.java
+++ WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/util/HttpUtil.java
@@ -2,13 +2,12 @@ package za.co.willemvisser.wpvhomecontroller.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 
 public enum HttpUtil {
@@ -18,16 +17,18 @@ public enum HttpUtil {
 	static Logger log = Logger.getLogger(HttpUtil.class.getName());
 	
 	
+	/**
+	 * Given a url, do a HTTP GET request and return the response
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
 	public HttpResponse doHttpGet(String url) throws IOException {
-		HttpClient httpclient = HttpClientBuilder.create().build();
-
-        HttpGet httpget = new HttpGet( url );
-
-        HttpResponse response;        
 		
-		response = httpclient.execute(httpget);
-		
-		
+		HttpClient httpClient = HttpClients.custom().setUserAgent("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19").build();	    		
+        HttpGet httpget = new HttpGet( url );        
+        HttpResponse response = httpClient.execute(httpget);
+				
 		log.debug("doHttpGet (" + url + ") Response Status: " + response.getStatusLine());		        
 
         return response;
