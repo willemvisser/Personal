3ddc7c917612996cf3279d6025f338c25290d66a
Willem Visser <willem.visser@gmail.com>
2020-09-17 09:12:36 +0200

    Moving AWS client builder logic into Job execution context

--- WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/scheduler/job/WaterTankPostMetricsJob.java
+++ WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/scheduler/job/WaterTankPostMetricsJob.java
@@ -26,13 +26,14 @@ public class WaterTankPostMetricsJob implements InterruptableJob {
 
 	static Logger log = Logger.getLogger(WaterTankPostMetricsJob.class.getName());
 	
-	final AmazonCloudWatch cw =
-		    AmazonCloudWatchClientBuilder.defaultClient();
+	
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 					
 		try {
+			AmazonCloudWatch cw = AmazonCloudWatchClientBuilder.defaultClient();
+			
 			StringBuffer response = HttpUtil.INSTANCE.getResponseContent(HttpUtil.INSTANCE.doHttpGet(
 					ConfigController.INSTANCE.getGeneralProperty(ConfigController.PROPERTY_TANK_LEVEL_HTTP_URL).getValue()));
 						
@@ -48,41 +49,41 @@ public class WaterTankPostMetricsJob implements InterruptableJob {
 			}
 						
 				
-				Collection<Dimension> dimensions = new ArrayList<Dimension>();
+			Collection<Dimension> dimensions = new ArrayList<Dimension>();
 			
-				Dimension dimensionHostName = new Dimension()
-				    .withName("Host Name")
-				    .withValue(ConfigController.INSTANCE.getHostName());
-				dimensions.add(dimensionHostName);
-				
-				GeneralPropertyDTO propDTO = ConfigController.INSTANCE.getGeneralProperty("ServerType");
-				if (propDTO != null) {
-					Dimension dimensionServerType = new Dimension()
-				    	.withName("Server Type")
-				    	.withValue(propDTO.getValue());
-					dimensions.add(dimensionServerType);
-				}
-				
-				MetricDatum datum = new MetricDatum()
-				    .withMetricName("Water Tank Depth")
+			Dimension dimensionHostName = new Dimension()
+			    .withName("Host Name")
+			    .withValue(ConfigController.INSTANCE.getHostName());
+			dimensions.add(dimensionHostName);
+			
+			GeneralPropertyDTO propDTO = ConfigController.INSTANCE.getGeneralProperty("ServerType");
+			if (propDTO != null) {
+				Dimension dimensionServerType = new Dimension()
+			    	.withName("Server Type")
+			    	.withValue(propDTO.getValue());
+				dimensions.add(dimensionServerType);
+			}
+			
+			MetricDatum datum = new MetricDatum()
+			    .withMetricName("Water Tank Depth")
+			    .withUnit(StandardUnit.None)
+			    .withValue(currentDepth)
+			    .withDimensions(dimensions);
+			
+			MetricDatum datum2 = new MetricDatum()
+				    .withMetricName("Water Tank Percentage Full")
 				    .withUnit(StandardUnit.None)
-				    .withValue(currentDepth)
+				    .withValue(percentageFull)
 				    .withDimensions(dimensions);
-				
-				MetricDatum datum2 = new MetricDatum()
-					    .withMetricName("Water Tank Percentage Full")
-					    .withUnit(StandardUnit.None)
-					    .withValue(percentageFull)
-					    .withDimensions(dimensions);
 
-				PutMetricDataRequest request = new PutMetricDataRequest()
-				    .withNamespace("HomeAutomation")
-				    .withMetricData(datum)
-				    .withMetricData(datum2);
+			PutMetricDataRequest request = new PutMetricDataRequest()
+			    .withNamespace("HomeAutomation")
+			    .withMetricData(datum)
+			    .withMetricData(datum2);
 
-				cw.putMetricData(request);
-				
-				log.debug("Tank metrics posted to CloudWatch");
+			cw.putMetricData(request);			
+			
+			log.debug("Tank metrics posted to CloudWatch");
 			
 		} catch (Exception e) {
 			log.error("Water Tank Post metrics error (posting to AWS: " + e);
