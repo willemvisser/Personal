47b43938e1f79e96aa1fe352948d79a09e6e6a0e
Willem Visser <willem.visser@gmail.com>
2020-09-23 23:13:05 +0200

    New PropertyManager

--- WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/scheduler/WPVHomeControllerScheduler.java
+++ WPVHomeController_Scheduler_WebApp/src/za/co/willemvisser/wpvhomecontroller/scheduler/WPVHomeControllerScheduler.java
@@ -39,8 +39,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+//import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
